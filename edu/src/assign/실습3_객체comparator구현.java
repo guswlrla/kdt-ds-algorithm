a39abbe85e1fd81d0876922634b668fc9f83f182
Kim Hyeon Ji <rlaguswl_01@naver.com>
2025-09-26 02:29:18 +0900

    과제3완

--- edu/src/assign/실습3_객체comparator구현.java
+++ edu/src/assign/실습3_객체comparator구현.java
@@ -35,16 +35,14 @@ class Student {
 }
 
 public class 실습3_객체comparator구현 {
-	
 	private static void show(Student[] arr) {
 		System.out.println("=== 정렬 전 학생 목록 ===");
 		for(Student d : arr) {
 			System.out.println(d.toString());
 		}
+		System.out.println();
 	}
 
-	
-	
 	public static void main(String[] args) {
 		Student[] students = {
 			    new Student("S001", "영희", "Math"),
@@ -56,40 +54,25 @@ public class 실습3_객체comparator구현 {
 
 		show(students);
 		
-//		Comparator<Student> sidComparator = (a, b) -> compare(a.getSid(), b.getSid());
-//		Arrays.sort(students, sidComparator);
+		Comparator<Student> comparator = (a, b) -> a.getSid().compareTo(b.getSid()); // 객체형을 비교하려면 compareTo()를 사용
+		Arrays.sort(students, comparator);
+		
+		Student[] targets = {
+			    new Student("S002", "철수", "Physics"),
+			    new Student("S006", "홍길동", "Law"),
+			    new Student("S004", "준호", "Chemistry")
+			};
+		
+		System.out.println("=== 이진 탐색 결과 ===");
+		for(Student key : targets) {
+			int index = Arrays.binarySearch(students, key, comparator);
+			
+			if(index > 0) {
+				System.out.println("찾은 학생 : " + students[index]);
+			}
+			else {
+				System.out.println("학번 " + key.getSid() + "인 학생은 존재하지 않습니다.");
+			}
+		}
 	}
-/*
- * Comparator를 사용한 학생 정보 배열 정렬 및 이진 탐색 구현
- * 
- * 고정된 학생 배열을 Comparator를 사용해 학번(sid) 기준으로 정렬한 뒤,
- * 탐색 대상 학생 객체 배열에 대해 Arrays.binarySearch()를 사용하여 
- * 해당 학번을 가진 학생이 존재하는지 판단하는 프로그램을 작성하시오.
- * 
- * 구현 조건
-
-
-다음 탐색 대상 배열을 정의하고, 확장형 for문으로 각 항목에 대해 Arrays.binarySearch(배열, key, comparator)로 탐색한다.
-
-Student[] targets = {
-    new Student("S002", "철수", "Physics"),
-    new Student("S006", "홍길동", "Law"),
-    new Student("S004", "준호", "Chemistry")
-};
-
-탐색 결과는 다음과 같이 출력한다:
-
-"찾은 학생: S002, 철수, Physics"
-
-"학번 S006인 학생은 존재하지 않습니다."
-
-=== 이진 탐색 결과 ===
-찾은 학생: S002, 철수, Physics
-학번 S006인 학생은 존재하지 않습니다.
-찾은 학생: S004, 준호, Chemistry
-
- * 
- */
-
-
 }
