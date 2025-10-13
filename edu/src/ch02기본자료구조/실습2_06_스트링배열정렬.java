4036c853fdc6c229b2d53024445f2ab10a7145a1
Kim Hyeon Ji <rlaguswl_01@naver.com>
2025-10-13 18:09:58 +0900

    2장 마무리, 5장과제 공부

--- edu/src/ch02기본자료구조/실습2_06_스트링배열정렬.java
+++ edu/src/ch02기본자료구조/실습2_06_스트링배열정렬.java
@@ -12,10 +12,39 @@
 //
 //		showData("정렬전", data);
 //		// Arrays.sort(data); // Internally uses String.compareTo()
-//		Arrays.sort(data, (s1, s2) -> s1.compareTo(s2));
+//		// Arrays.sort(data, (s1, s2) -> s1.compareTo(s2));
 //		sortData(data);
 //		showData("정렬후", data);
 //		String[] newData = insertString(data, "banana");
 //		showData("삽입후 크기가 증가된 정렬 배열", newData);
+//	}
+//
+//	private static String[] insertString(String[] data, String string) {
+//		int len = data.length;
+//		String arr[] = new String[len + 1];
+//		
+//		int i = 0;
+//		while(data[i] <= ) { // 인덱스를 들고 나와야함
+//			arr[i] = data[i];
+//			i++;
+//		}
+//		arr[i] = string;
+//		return arr;
+//	}
+//
+//	private static void sortData(String[] data) {
+//		for(int i = 0; i < data.length-1; i ++) {
+//			for(int j = 0; j < data.length-1-i; j++) {
+//				if(data[j].compareTo(data[j+1]) > 0) {
+//					String temp = data[j];
+//					data[j] = data[j+1];
+//					data[j+1] = temp;
+//				}
+//			}
+//		}
+//	}
+//
+//	private static void showData(String str, String[] data) {
+//		System.out.println(str + " : " + Arrays.toString(data));
 //	}
 //}
